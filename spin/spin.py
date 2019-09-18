a9576afa41110dce15f1697fe5ff2de671cd2a21
Otávio Vasques <otaviocv.deluqui@gmail.com>
2019-09-17 23:49:32 -0300

    Add side to side prototype

--- spin/spin.py
+++ spin/spin.py
@@ -1,5 +1,27 @@
 import numpy as np
 
+def side_to_side(distances, strictly_increasing_vector, max_iter=100):
+    X = strictly_increasing_vector
+    permutation = np.identity(distances.shape[0])
+    for i in range(max_iter):
+        S = distances.dot(X)
+        reverse_index_sort = (S).argsort()[::-1]
+        new_permutation = np.identity(distances.shape[0])[reverse_index_sort]
+        if new_permutation.dot(S) == permutation.dot(S):
+            break
+        permutation = new_permutation
+        X = permutation.dot(X)
+    return permutation.dot(distances).dot(permutation.T)
+
+
+def cost_function(distances, permutation, weight):
+    return (permutation.dot(distances)
+                       .dot(permutation.T)
+                       .dot(weight)).trace()
+
+def weight_matrix(strictly_increasing_vector):
+    return strictily_increasing_vector.dot(strictly_increasing_vector.T)
+
 
 def general_distance_matrix(X, dist_function):
     n = X.shape[1]
@@ -11,9 +33,11 @@ def general_distance_matrix(X, dist_function):
             dist_matrix[j,i] = dist
         
 
-def l2_distance_matrix(X):
-    dists = (np.dot(X.T, X))
-    return dists
+def l2_distance_matrix(X, Y):
+    dists = -2 * X.T.dot(Y) + \
+            np.sum(X**2, axis=0) + \
+            np.sum(Y**2, axis=0).reshape(1, -1).T
+    return np.sqrt(dists)
 
-def l1_distance_matrix(X):
+def l1_distance_matrix(X, Y):
     pass
