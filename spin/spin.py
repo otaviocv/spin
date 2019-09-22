882a2e03e3bbcf87e883f31aa6c08ebd46580497
Otávio Vasques <otaviocv.deluqui@gmail.com>
2019-09-21 22:55:25 -0300

    Add examples and neighborhood spin prototype
    With this commit we have two examples using side to side spin.
    I also add the neighborhood spin prototype.

--- spin/spin.py
+++ spin/spin.py
@@ -1,17 +1,48 @@
 import numpy as np
 
+def neighborhood(distances, weight_matrix, max_iter=100):
+    permutation = np.identity(distances.shape[0])
+    W = weight_matrix
+    M = distances.dot(W)
+    for i in range(max_iter):
+        new_M = distances.dot(W)
+        new_permutation = argmin_(M)
+        if permutation.dot(M).trace() != new_permutation.dot(new_M).trace():
+            W = permutation.T.dot(W)
+    return permutation
+
+def argmin_(M):
+    return M
+
 def side_to_side(distances, strictly_increasing_vector, max_iter=100):
+    """ Side To Side SPIN algorithm
+
+    Parameters
+    ----------
+    distances: np.array, shape [n, n]
+        Distance symmetric square matrix.
+    strictly_increasing_vector: np.array, shape [n]
+        A vector with strictly increasing elements with the same dimension as
+        the distance matrix.
+    max_iter: int, default=100
+        Maximum number of iterations.
+
+    Returns
+    -------
+    permutation: np.array, shape [n, n]
+        Permutation matrix with the same dimensions of the distance matrix.
+    """
     X = strictly_increasing_vector
     permutation = np.identity(distances.shape[0])
     for i in range(max_iter):
         S = distances.dot(X)
         reverse_index_sort = (S).argsort()[::-1]
         new_permutation = np.identity(distances.shape[0])[reverse_index_sort]
-        if new_permutation.dot(S) == permutation.dot(S):
+        if np.all(new_permutation.dot(S) == permutation.dot(S)):
             break
         permutation = new_permutation
         X = permutation.dot(X)
-    return permutation.dot(distances).dot(permutation.T)
+    return permutation
 
 
 def cost_function(distances, permutation, weight):
@@ -37,6 +68,7 @@ def l2_distance_matrix(X, Y):
     dists = -2 * X.T.dot(Y) + \
             np.sum(X**2, axis=0) + \
             np.sum(Y**2, axis=0).reshape(1, -1).T
+    dists[dists < 0] = 0
     return np.sqrt(dists)
 
 def l1_distance_matrix(X, Y):
