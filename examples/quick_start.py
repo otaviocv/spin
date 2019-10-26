ab3a81fd898a05d2eedd536510fc3d72d270b188
otaviocv <otaviocv.deluqui@gmail.com>
2019-10-25 22:20:16 -0300

    Add quick start example

---
+++ examples/quick_start.py
@@ -0,0 +1,18 @@
+from spin import NeighborhoodSPIN
+from spin.distances import l2_distance_matrix
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+
+# Loading data
+data = load_iris()
+X = data.data
+y = data.target
+
+distances = l2_distance_matrix(X.T, X.T) # The l2 distance matrix computes the
+                                         # l2 norm for two sets of column
+                                         # vectors, so lets compute all
+                                         # distances from all data points.
+spin = NeighborhoodSPIN()
+spin.run(X)
+
+plt.matshow(spin.ordered_distances_)
